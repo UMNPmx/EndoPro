e0a99b7fcc9f6faaf2abbd81265201b5738bce2d
Mutaz Jaber <mutazjab94@gmail.com>
2020-02-23 12:22:26 -0600

    single cosine

---
+++ R/single_cosine.R
@@ -0,0 +1,28 @@
+#' Single cosine model
+#'
+#' Estimation of Amp, mean concentration
+#'
+#' @param t time
+#' @param y Observed concentration
+#' @param p Initial values. p1: Amplitude p2: mean endogenous rate p3: acrophase time
+#'
+#' @export single_cos Single cosine function
+
+
+single_cos <- function(y, t, p) {
+  pred <- p[1] + p[2] * cos(2 * pi * (t - p[3]) / 24)
+
+  rss <- function(y, pred, t) {
+    sum(((y - pred)) ^ 2)
+  }
+    fr <- stats::nlm(rss,
+                     p = p,
+                     t = t,
+                     y = y)
+
+    return(fr$estimate)
+  }
+
+
+
+
