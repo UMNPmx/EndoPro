e78b71eb47b740035aa9eab3363caf51ffe04f32
Mutaz M. Jaber <mutazjab94@gmail.com>
2020-02-24 12:44:10 -0600

    new

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Endogenous substance concnetration level
+#'
+#'
+#'
+#' @format Data for endogenous substance
+#' \describe{
+#' \item{time}{time in hours}
+#' \item{DV}{ Observed Concentration}
+#' \item{Norm} {Normalized observed concnetration}
+#' }
+#'
+#'
+NULL
